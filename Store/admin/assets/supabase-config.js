e8988167ecbf16fdd0fc41cfe4e7b0e74ae4d70c
Mostafahamada-cpu <135072648+Mostafahamada-cpu@users.noreply.github.com>
2026-07-07 15:27:51 +0300

    Update supabase-config.js

--- Store/admin/assets/supabase-config.js
+++ Store/admin/assets/supabase-config.js
@@ -1,5 +1,5 @@
 window.MEMORA_SUPABASE = {
-  url: 'https://YOUR_PROJECT_REF.supabase.co',
-  anonKey: 'YOUR_SUPABASE_ANON_KEY',
+  url:'https://dwsltglmmatbqtgliygd.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3c2x0Z2xtbWF0YnF0Z2xpeWdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0MTUwMzUsImV4cCI6MjA5ODk5MTAzNX0.j9wGN9X6SRf5AVIlaey8PwzQ9lEt24faQDk1Gve9eyo',
   storageBucket: 'memora-assets'
 };
